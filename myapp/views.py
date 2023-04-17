61d3d3302b4769cba2c79d6e5d2d77de614f8b61
Cristian Rivera <cristian.rivera3284@gmail.com>
2023-04-17 17:23:21 -0400

    Segundo commit, curso Django <Fazt>

--- myapp/views.py
+++ myapp/views.py
@@ -3,8 +3,13 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+def hello(request, username):
+    print(type(username))
+    return HttpResponse("<h1>Hello %s</h1>" % username)
 
 def about(request):
     return HttpResponse('About')
+
+def index(request):
+    return HttpResponse('Page Index')
+    
